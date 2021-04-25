25ddf13a271953b25e6f865c5840eeb2db677bd9
Kevin Devlin <azwethinkweizkd@gmail.com>
2021-04-24 20:31:28 -0600

    Added prompt in Javascript that requested password length and then store that value.

--- script.js
+++ script.js
@@ -1,6 +1,99 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var choiceUpper = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var choiceLower = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var choiceNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var choiceSpec = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "&",
+  "?",
+  "~",
+  "-",
+  "_",
+  "{",
+  "}",
+  "[",
+  "]",
+];
+var passwordString = "";
+
+function generatePassword() {
+  //Capture User Inputs
+  //Generate the password.
+  var passwordLength = prompt(
+    "How long would you like your password to be? (Min: 8, Max: 128)"
+  );
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert(
+      "Password is not within set length parameters, please select again"
+    );
+    generatePassword();
+  } else passwordLength >= 8 || passwordLength <= 128;
+
+  // var randomUpper = confirm(
+  //   "Would you like to include upper case letters in your password?"
+  // );
+  // if (true) {
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,7 +105,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword() {
-  //Capture User Inputs
-  //Generate the password.
-}
+writePassword();
